b1edadeed1c79a7686f7dd20ed7b6733c3e7105e
Arthy17 <arthyarumugam18@gmail.com>
2023-11-17 22:59:34 -0800

    first

---
+++ Question set 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_set_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static void Qset2_1()
+        {
+        }
+    }
+}
